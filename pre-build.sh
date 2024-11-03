302674149f925983af13d47f479da9b4a59ee935
SharkyEXE <32225526+SharkyEXE@users.noreply.github.com>
2024-11-03 16:53:35 +0300

    Update pre-build.sh

--- pre-build.sh
+++ pre-build.sh
@@ -1,4 +1,4 @@
-ln -snf /usr/share/zoneinfo/Europe/Kyiv /etc/localtime
+ln -snf /usr/share/zoneinfo/Asia/Kuwait /etc/localtime
 apt update
 DEBIAN_FRONTEND=noninteractive apt install tzdata -y
 date
