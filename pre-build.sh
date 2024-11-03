e014ea22dadb2eca1bd489afe97803b533898057
SharkyEXE <32225526+SharkyEXE@users.noreply.github.com>
2024-11-03 15:22:01 +0300

    Update pre-build.sh

--- pre-build.sh
+++ pre-build.sh
@@ -1,5 +1,2 @@
-# Set timezone:
-RUN ln -snf /usr/share/zoneinfo/Asia/Kuwait /etc/localtime && echo Asia/Kuwait > /etc/timezone
-
-# Install dependencies:
-RUN apt-get update && apt-get install -y tzdata
+ln -snf /usr/share/zoneinfo/Asia/Kuwait /etc/localtime && echo Asia/Kuwait > /etc/timezone
+apt-get update && apt-get install -y tzdata
