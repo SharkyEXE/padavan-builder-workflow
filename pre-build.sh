05bb52677efc01843615abe5ab86c144c3445e08
SharkyEXE <32225526+SharkyEXE@users.noreply.github.com>
2024-11-03 15:33:38 +0300

    Update pre-build.sh

--- pre-build.sh
+++ pre-build.sh
@@ -1,3 +1,1 @@
-ln -fs /usr/share/zoneinfo/America/New_York /etc/localtime
-DEBIAN_FRONTEND=noninteractive apt-get install -y tzdata
-dpkg-reconfigure --frontend noninteractive tzdata
+sudo timedatectl set-timezone America/New_York
